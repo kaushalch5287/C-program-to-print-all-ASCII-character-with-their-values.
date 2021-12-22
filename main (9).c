9006cad2f9034e223d8d27fc100384033f64b714
KAUSHAL CHAUHAN <93530886+kaushalch5287@users.noreply.github.com>
2021-12-22 18:37:17 +0530

    Add files via upload

---
+++ main (9).c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{
+ int i;
+ for(i=0; i<=255; i++) 
+ {
+ printf("ASCII value of character %c = %d\n", i, i);
+ }
+ return 0;
+}
+
+
